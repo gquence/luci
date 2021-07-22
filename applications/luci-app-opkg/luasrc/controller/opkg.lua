8de9b787e9da9f9181e56906a0e27fd377b96651
Mikhail Anshukov <ma.anshukov@gmail.com>
2021-07-22 15:23:42 +0300

    luci-app-opkg: changed opkg.lua page-entry to menu-json file from luci 21.02 for using rpcd-acl-groups; added groups of basic rules for this module
    Signed-off-by: Mikhail Anshukov <ma.anshukov@gmail.com>

--- applications/luci-app-opkg/luasrc/controller/opkg.lua
+++ applications/luci-app-opkg/luasrc/controller/opkg.lua
@@ -4,7 +4,6 @@
 module("luci.controller.opkg", package.seeall)
 
 function index()
-	entry({"admin", "system", "opkg"}, template("opkg"), _("Software"), 30)
 	entry({"admin", "system", "opkg", "list"}, call("action_list")).leaf = true
 	entry({"admin", "system", "opkg", "exec"}, post("action_exec")).leaf = true
 	entry({"admin", "system", "opkg", "statvfs"}, call("action_statvfs")).leaf = true
